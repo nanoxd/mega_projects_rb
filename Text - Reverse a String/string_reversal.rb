da72cb4497f46c03e52402e47e6073fa4f458f3e
Fernando Paredes <nanoid.xd@gmail.com>
2013-07-08 13:33:22 -0700

    [x] Text - Reverse a String

---
+++ Text - Reverse a String/string_reversal.rb
@@ -0,0 +1,7 @@
+# Title: Reverse a String
+# Description:  Enter a string and the program will reverse it and print it out.
+
+# A simple implementation that asks for a string and reverses it.
+print "Please enter a word: "
+word = gets.chop!
+puts word.reverse
